516dee6bfcc4c3524c87aefe8ca3fde1c91e2099
donhyun <donhyun@hotmail.com>
2024-06-08 21:57:58 -0700

    fixed ProjectSettings.  Added Wallet to tanks.

--- Assets/Scripts/Coins/CoinWallet.cs
+++ Assets/Scripts/Coins/CoinWallet.cs
@@ -9,12 +9,15 @@ public class CoinWallet : NetworkBehaviour
 
     private void OnTriggerEnter(Collider col)
     {
+        Debug.Log("Coin Trigger Enter");
         if (!col.TryGetComponent<Coin>(out Coin coin)) { return; }
 
+        Debug.Log("Collect!");
         int coinValue = coin.Collect();
 
         if (!IsServer) { return; }
 
         TotalCoins.Value += coinValue;
+        Debug.Log("Total Coin Value:" + TotalCoins.Value);
     }
 }
