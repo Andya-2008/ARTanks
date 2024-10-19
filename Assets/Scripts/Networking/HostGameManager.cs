71eafb1b8872eab7d5359cc6e74c9e00b545f123
donhyun <donhyun@hotmail.com>
2024-10-18 20:05:58 -0700

    automatic joincode dropdown

--- Assets/Scripts/Networking/HostGameManager.cs
+++ Assets/Scripts/Networking/HostGameManager.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -17,6 +19,23 @@ public class HostGameManager
 
     private const int MaxConnections = 20;
     private const string GameSceneName = "Game";
+
+
+    // Lobby data key used to lookup each players' name as displayed in the lobby.
+    public const string k_PlayerNameKey = "playerName";
+
+    // Lobby data key used to check if each player has clicked the [Ready] button.
+    public const string k_IsReadyKey = "isReady";
+
+    // Lobby data for host name.
+    public const string k_HostNameKey = "hostName";
+
+    // Lobby data for host's Relay Join Code. Used to allow all players to initialize Relay so NGO
+    // (Netcode for GameObjects) can synchronize multiplayer game play between players.
+    public const string k_RelayJoinCodeKey = "relayJoinCode";
+
+
+    public Lobby activeLobby { get; private set; }
 
     public async Task StartHostAsync()
     {
@@ -41,6 +60,23 @@ public class HostGameManager
             return;
         }
 
+
+
+        var options = new CreateLobbyOptions();
+        options.IsPrivate = false;
+        options.Data = new Dictionary<string, DataObject>
+                {
+                    { k_HostNameKey, new DataObject(DataObject.VisibilityOptions.Public, joinCode) },
+                    { k_RelayJoinCodeKey, new DataObject(DataObject.VisibilityOptions.Public, joinCode) },
+                };
+
+        options.Player = CreatePlayerData();
+
+        string lobbyName = joinCode;
+        int maxPlayers = 4;
+
+        activeLobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, options);
+
         UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
 
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
@@ -50,6 +86,27 @@ public class HostGameManager
 
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
         GameObject.Find("RoomCodeCanvas").GetComponent<RoomCodeText>().RoomCode(joinCode);
+    }
+
+
+
+    Player CreatePlayerData()
+    {
+        var player = new Player();
+        player.Data = CreatePlayerDictionary();
+
+        return player;
+    }
+
+    Dictionary<string, PlayerDataObject> CreatePlayerDictionary()
+    {
+        var playerDictionary = new Dictionary<string, PlayerDataObject>
+            {
+                { k_PlayerNameKey,  new PlayerDataObject(PlayerDataObject.VisibilityOptions.Public, "Playername") },
+                { k_IsReadyKey,  new PlayerDataObject(PlayerDataObject.VisibilityOptions.Public, "True") },
+            };
+
+        return playerDictionary;
     }
 }
 
--- Assets/Scripts/Networking/MainMenu.cs
+++ Assets/Scripts/Networking/MainMenu.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_InputField joinCodeField;
-
+    [SerializeField] private TMP_Dropdown joinCodeDropDown;
+    private float lastCheck = 0;
     public async void StartHost()
     {
         await HostSingleton.Instance.GameManager.StartHostAsync();
@@ -14,6 +18,69 @@ public class MainMenu : MonoBehaviour
 
     public async void StartClient()
     {
-        await ClientSingleton.Instance.GameManager.StartClientAsync(joinCodeField.text);
+        string joincode;
+        if (joinCodeField.text != "")
+        {
+            joincode = joinCodeField.text;
+        }
+        else {
+            joincode = joinCodeDropDown.options[joinCodeDropDown.value].text;
+        }
+
+        await ClientSingleton.Instance.GameManager.StartClientAsync(joincode);
+    }
+
+	private void Update()
+	{
+
+	}
+
+	public async void UpdateLobbies() {
+        Debug.Log("UpdateLobbies");
+        try
+        {
+
+            //await UnityServices.InitializeAsync();
+            QueryLobbiesOptions options = new QueryLobbiesOptions();
+            options.Count = 25;
+
+            // Filter for open lobbies only
+            options.Filters = new List<QueryFilter>()
+            {
+                new QueryFilter(
+                    field: QueryFilter.FieldOptions.AvailableSlots,
+                    op: QueryFilter.OpOptions.GT,
+                    value: "0")
+            };
+
+                    // Order by newest lobbies first
+                    options.Order = new List<QueryOrder>()
+            {
+                new QueryOrder(
+                    asc: false,
+                    field: QueryOrder.FieldOptions.Created)
+            };
+
+            QueryResponse lobbies = await LobbyService.Instance.QueryLobbiesAsync(options);
+            Debug.Log("Got Lobbies:" + lobbies.Results.Count);
+
+            joinCodeDropDown.options.Clear();
+            List<string> rooms = new List<string>();
+            foreach (var l in lobbies.Results) {
+                Debug.Log(l.Name + ":" + l.LobbyCode);
+                rooms.Add(l.Name);
+                /*
+                foreach (var d in l.Data) {
+                    Debug.Log(d.Key + ":" + d.Value);
+                }*/
+
+
+            }
+            joinCodeDropDown.AddOptions(rooms);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
     }
 }
